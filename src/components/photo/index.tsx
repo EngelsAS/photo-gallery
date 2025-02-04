86ea473535d27d2aac68105d7093b263f054fc4b
EngelsAS <engelsantero@gmail.com>
2025-02-04 14:30:25 -0300

    add: blurhash optimized loading

--- src/components/photo/index.tsx
+++ src/components/photo/index.tsx
@@ -4,6 +4,16 @@ import { Basic } from "unsplash-js/dist/methods/photos/types";
 
 const Photo = ({ data }: { data: Basic }) => {
   const [isLoaded, setIsLoaded] = useState(false);
+  const [blurHeight, setBlurHeight] = useState(0);
+  const divRef = useRef<HTMLDivElement>(null);
+
+  const handleResizeDiv = () => {
+    if (divRef.current) {
+      const currentHeight = divRef.current.offsetWidth;
+      const newHeight = (data.height / data.width) * currentHeight;
+      setBlurHeight(newHeight);
+    }
+  };
 
   useEffect(() => {
     const img = new Image();
@@ -15,30 +25,43 @@ const Photo = ({ data }: { data: Basic }) => {
     img.src = data.urls.small;
   }, [data]);
 
-  const divRef = useRef(null);
+  useEffect(() => {
+    const resizeObserver = new ResizeObserver(handleResizeDiv);
+
+    if (divRef.current) {
+      resizeObserver.observe(divRef.current);
+    }
+
+    return () => {
+      resizeObserver.disconnect();
+    };
+
+    //eslint-disable-next-line
+  }, []);
 
   return (
-    <div className="w-full relative min-h-60">
-      {!isLoaded && (
-        <div className="w-full h-full absolute" ref={divRef}>
-          <Blurhash
-            hash={data.blur_hash || ""}
-            width="100%"
-            height={"100%"}
-            resolutionX={32}
-            resolutionY={32}
-            punch={1}
-          />
-        </div>
-      )}
-
-      {isLoaded && (
-        <img
-          className="w-full object-contain"
-          loading="lazy"
-          src={data.urls.small}
+    <div
+      className="w-full relative"
+      ref={divRef}
+      style={{ height: blurHeight }}
+    >
+      <div
+        className={`w-full h-full absolute ${!isLoaded ? "block" : "hidden"}`}
+      >
+        <Blurhash
+          hash={data.blur_hash || ""}
+          width="100%"
+          height="100%"
+          resolutionX={32}
+          resolutionY={32}
+          punch={1}
         />
-      )}
+      </div>
+
+      <img
+        className={`w-full object-contain ${isLoaded ? "block" : "hidden"}`}
+        src={data.urls.small}
+      />
     </div>
   );
 };
